0399c67025829931976ecc75ba5a4afaebe4aece
LaniJeong <lani03774@gamil.com>
2023-01-30 17:23:22 +0900

    별표 찍기
    단축어

---
+++ Day01/code01_start_print.py
@@ -0,0 +1,12 @@
+# date : 2023-01-30
+# 별표찍기
+# ctrl+c / v : 바로 아래 복붙
+# Alt+up/down : 위, 아래로 이동
+# ctrl+s : 저장(생활화하기)
+print('*Merry Christmas*')
+print('    *')
+print('   ***')
+print('  *****')
+print(' *******')
+print('*********')
+print('    I')
